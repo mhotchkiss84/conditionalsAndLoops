c99c541a8d9c70f7f4bfedd8933f54e13861f5fd
Michael <sxs.linux@hotmail.com>
2020-03-21 11:18:45 -0400

    html and javascript file created

---
+++ script.js
@@ -0,0 +1,25 @@
+// var likesToTravel = true;
+
+// // Checking if likesToTravel is equal to true
+// if (likesToTravel === true) {
+//     console.log("Bon voyage");
+// } else {
+//     console.log("Enjoy your couch.");
+// }
+
+// // Testing not true with !=
+// if (likesToTravel !== true){
+//     console.log("Enjoy your couch");
+// } else {
+//     console.log("Bon voyage")
+// }
+
+
+// In your JavaScript file, declare a new variable called favoriteFood and set it equal to your favorite food.
+var favoriteFood = "pepperoni rolls";
+// Write a conditional that checks to see if the favoriteFood variable is equal to "fried pickles" OR "pepperoni rolls". If so, log "Congratulations! You have excellent taste!" to the console. Otherwise, log "Clearly you have not tried fried pickles or pepperoni rolls." to the console.
+if(favoriteFood === "fried pickles" || favoriteFood === "pepperoni rolls"){
+    console.log("Congratulations! you have excellent taste!");
+} else{
+    console.log("Clearly you have not tried fried pickles or pepperoni roles");
+}
